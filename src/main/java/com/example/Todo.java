0b9f2c787f6752b0bb4e33ffeeaa77df3e62470e
woooyoung <axdsw121@gmail.com>
2025-09-15 11:54:12 +0900

    todo CRUD

---
+++ src/main/java/com/example/Todo.java
@@ -0,0 +1,28 @@
+package com.example;
+
+public class Todo {
+
+    private long id;
+    private String content;
+
+    public Todo(long id,String content){
+        this.id = id;
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
